4f63d4b333f9eb80087ebf3730ff95629ef9432b
Tan Xin Jie <vennusbear@gmail.com>
2019-01-04 22:15:47 +0800

    off rigidbody interpolation + move some script

--- Assets/Scripts/PlateScript.cs
+++ Assets/Scripts/PlateScript.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
 
 public class PlateScript : MonoBehaviour {
 
+    VRTK_InteractableObject vrtkIntObj;
+    Rigidbody plateRB;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        vrtkIntObj = GetComponent<VRTK_InteractableObject>();
+        plateRB = GetComponent<Rigidbody>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
